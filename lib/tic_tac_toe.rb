13fa5e8aaba6f54639148720fa0b7dcb5633d401
Mary Dick <maryclairedick@gmail.com>
2020-07-21 20:32:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,7 +95,7 @@ def play(board)
   end
    
   if won?(board)
-    puts "Congratulations #{current_player(board)}!"
+    puts "Congratulations #{winner}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
