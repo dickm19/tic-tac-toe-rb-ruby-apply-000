d123b199fac39877cd94e5d2e6c7adbc8c027d64
Mary Dick <maryclairedick@gmail.com>
2020-07-21 19:54:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,10 +74,7 @@ def valid_move?(board, move)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
- 
+  valid_move(board)
   until !valid_move?(board, index) do
     
     puts "Please enter 1-9:"
